9f9d1c11eb8d62fd5f4efebc821256aacb22fb6d
SaiBalaji202 <saibalaji202@gmail.com>
2018-09-15 23:29:40 +0530

    first commit

---
+++ WebCam1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WebCam1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picOutput = new System.Windows.Forms.PictureBox();
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.picStream = new System.Windows.Forms.PictureBox();
+            this.btnStreamOnOff = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStream)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picOutput
+            // 
+            this.picOutput.Location = new System.Drawing.Point(374, 34);
+            this.picOutput.Name = "picOutput";
+            this.picOutput.Size = new System.Drawing.Size(157, 163);
+            this.picOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picOutput.TabIndex = 0;
+            this.picOutput.TabStop = false;
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(51, 282);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(130, 23);
+            this.btnCapture.TabIndex = 1;
+            this.btnCapture.Text = "Capture";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // picStream
+            // 
+            this.picStream.Location = new System.Drawing.Point(35, 34);
+            this.picStream.Name = "picStream";
+            this.picStream.Size = new System.Drawing.Size(157, 163);
+            this.picStream.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picStream.TabIndex = 0;
+            this.picStream.TabStop = false;
+            // 
+            // btnStreamOnOff
+            // 
+            this.btnStreamOnOff.Location = new System.Drawing.Point(51, 230);
+            this.btnStreamOnOff.Name = "btnStreamOnOff";
+            this.btnStreamOnOff.Size = new System.Drawing.Size(130, 23);
+            this.btnStreamOnOff.TabIndex = 1;
+            this.btnStreamOnOff.Text = "Start Streaming";
+            this.btnStreamOnOff.UseVisualStyleBackColor = true;
+            this.btnStreamOnOff.Click += new System.EventHandler(this.btnStreamOnOff_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(415, 230);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 446);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStreamOnOff);
+            this.Controls.Add(this.btnCapture);
+            this.Controls.Add(this.picStream);
+            this.Controls.Add(this.picOutput);
+            this.Name = "Form1";
+            this.Text = "Camera";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStream)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picOutput;
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.PictureBox picStream;
+        private System.Windows.Forms.Button btnStreamOnOff;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ WebCam1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Windows.Forms;
+namespace WebCam1
+{
+    public partial class Form1 : Form
+    {
+        bool _streaming;
+        Capture _capture;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            _streaming = false;
+            _capture = new Capture();
+        }
+
+        private void btnStreamOnOff_Click(object sender, System.EventArgs e)
+        {
+            if (!_streaming)
+            {
+                Application.Idle += streaming;
+                btnStreamOnOff.Text = @"Stop Streaming";
+            }
+            else
+            {
+                Application.Idle -= streaming;
+                btnStreamOnOff.Text = @"Start Streaming";
+            }
+            _streaming = !_streaming;
+        }
+
+        private void btnCapture_Click(object sender, System.EventArgs e)
+        {
+            picOutput.Image = picStream.Image;
+        }
+
+        private void btnSave_Click(object sender, System.EventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = @"Save Your Photo";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                picOutput.Image.Save(saveFileDialog.FileName + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                MessageBox.Show(@"Saved!");
+            }
+        }
+
+        // Streaming Event Handler
+        private void streaming(object sender, System.EventArgs e)
+        {
+            var img = _capture.QueryFrame().ToImage<Bgr, byte>();
+            var bmp = img.Bitmap;
+            picStream.Image = bmp;
+        }
+
+    }
+}
